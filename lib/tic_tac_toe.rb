1f776ca6ddd825b58c40ff612551a35feb0148f3
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-13 02:32:35 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left side
+  [1,4,7], # Center vertical
+  [2,5,8], # Right Side
+  [2,4,6], # Diagnol 1
+  [0,4,8] # Diagnol 2
+]
